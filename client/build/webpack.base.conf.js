53d2f32d1b38ef59e841e63c4159b04c20638560
erguotou <erguotou525@gmail.com>
2017-02-21 05:55:20 +0000

    Auto commit

---
+++ client/build/webpack.base.conf.js
@@ -0,0 +1,89 @@
+var path = require('path')
+var webpack = require('webpack')
+var config = require('../../config').frontend
+var utils = require('./utils')
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, '../src/main.js')
+  },
+  output: {
+    path: config.assetsRoot,
+    publicPath: config.assetsPublicPath,
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.js', '.vue'],
+    enforceExtension: true,
+    modules: ['node_modules'],
+    alias: {
+      'vue$': 'vue/dist/vue.common.js',
+      'src': path.resolve(__dirname, '../src'),
+      'assets': path.resolve(__dirname, '../src/assets'),
+      'components': path.resolve(__dirname, '../src/components'),
+      'locales': path.resolve(__dirname, '../src/locales'),
+      'shared': path.resolve(__dirname, '../src/shared'),
+      'resources': path.resolve(__dirname, '../src/resources')
+    }
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        eslint: {
+          formatter: require('eslint-friendly-formatter')
+        },
+        vue: {
+          loaders: utils.cssLoaders({ sourceMap: config.cssSourceMap }),
+          postcss: [
+            require('autoprefixer')({
+              browsers: ['last 2 versions']
+            })
+          ]
+        }
+      }
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        enforce: 'pre',
+        loader: 'eslint-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        loader: 'vue-html-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      }
+    ]
+  }
+}
